fdfbd417800e4226311ba7ad458cca9023ad7897
Pedro Henrique Neves da Silva <ptersnow@gmail.com>
2023-08-04 19:36:35 -0400

    feat: added home, skills, about and contact info.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,7 +16,7 @@ export default function About() {
                 </p>
                 <p>Throughout my career, I have worked on a wide range of projects, from simple static websites to
                     complex enterprise-level applications. I am experienced in working with a variety of development
-                    tools and frameworks, including React, Angular, Vue.js, Node.js, and Laravel. I am always eager
+                    tools and frameworks, including React, Node.js, Laravel and Python. I am always eager
                     to learn and explore new technologies, and I am constantly seeking out opportunities to improve
                     my skills and knowledge.</p>
             </div>
